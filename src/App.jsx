a492a97bebb9673c82969a2e63282f2b32d170c4
Mihir Solanki <127624978+MihirSolankii@users.noreply.github.com>
2025-06-01 21:17:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,9 @@ import ProblemList from './component/ProblemList'
 import CodeRoom from './sharing/CodeRoom'
 import RoomSelection from './component/ws/RoomSelection'
 import CollaborationRoom from './component/ws/CollaborationRoom'
-export const API_URL = "http://localhost:3000";
+export const API_URL = window.location.hostname === 'localhost' 
+  ? "http://localhost:3000" 
+  : "https://leetcode-backend-yizw.onrender.com";
 function App() {
   const token = localStorage.getItem('token')
   return (
